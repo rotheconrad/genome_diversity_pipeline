c7888606805602262b516bba3833b0e3f4678674
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-10-06 02:16:28 -0400

    Report quality down to 0

--- scripts/08_02_gsp_qual.R
+++ scripts/08_02_gsp_qual.R
@@ -5,10 +5,11 @@ pdf('08_02_gsp_qual.pdf')
 layout(1:length(argv))
 for(i in argv){
   a <- read.table(i, sep = '\t', header = TRUE, row.names = 1)
-  barplot(t(a), beside = TRUE,
+  b <- barplot(t(a), beside = TRUE,
     col = c('lightblue', 'darkblue', 'red3', 'darkred'),
     ylab = 'Quality', main = i)
   abline(h = 50, lty = 3)
+  rect(min(b), 0, max(b), 50, border = 'black', col = rgb(0,0,0,1/2))
 }
 dev.off()
 
