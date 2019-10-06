eeab8906f4e5b6626610093e9c19bf298360ebf5
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-10-06 02:33:37 -0400

    Update 08_02_gsp_qual.R

--- scripts/08_02_gsp_qual.R
+++ scripts/08_02_gsp_qual.R
@@ -12,7 +12,7 @@ for(i in argv){
   rect(1, 0, right.end, 50, border = NA, col = rgb(1,1,1,3/4))
   arrows(x0 = 1, x1 = right.end, y0 = c(0,50), y1 = c(0,50), lty = c(1,3),
     length = 0)
-  ablines(v = seq(1, right.end, by = nrow(b)+1), col = grey(1/2))
+  abline(v = seq(1, right.end, by = nrow(b)+1), col = grey(1/2))
 }
 t <- dev.off()
 
