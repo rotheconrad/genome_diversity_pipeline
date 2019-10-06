c104a3af60510ee3c02f8716762b251d71271b69
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-10-06 02:54:13 -0400

    Summary by method for quality plots

--- scripts/08_02_gsp_qual.R
+++ scripts/08_02_gsp_qual.R
@@ -2,17 +2,23 @@
 
 argv <- commandArgs(trailingOnly = TRUE)
 pdf('08_02_gsp_qual.pdf')
-layout(1:length(argv))
+layout(matrix(1:(length(argv)*2), byrow = TRUE, ncol = 2), widths = c(3, 1))
+col <- c('lightblue', 'darkblue', 'red3', 'darkred')
 for(i in argv){
+  # Gspp
   a <- read.table(i, sep = '\t', header = TRUE, row.names = 1)
-  b <- barplot(t(a), beside = TRUE,
-    col = c('lightblue', 'darkblue', 'red3', 'darkred'),
-    ylab = 'Quality', main = i)
-  right.end <- ceiling(max(b))
-  rect(1, 0, right.end, 50, border = NA, col = rgb(1,1,1,3/4))
-  arrows(x0 = 1, x1 = right.end, y0 = c(0,50), y1 = c(0,50), lty = c(1,3),
-    length = 0)
-  abline(v = seq(1, right.end, by = nrow(b)+1), col = grey(1/2))
+  b <- barplot(t(a), beside = TRUE, las = 1,
+    col = col, ylab = 'Quality', main = i)
+  left.end <- min(b) - 1
+  right.end <- max(b) + 1
+  rect(left.end, 0, right.end, 50, border = NA, col = rgb(1,1,1,3/4))
+  arrows(x0 = left.end, x1 = right.end, y0 = c(0,50), y1 = c(0,50),
+    lty = c(1,3), length = 0)
+  abline(v = seq(left.end, right.end, by = nrow(b)+1), col = grey(1/2))
+  # Summaries
+  a.50 <- a
+  a.50[is.na(a.50) | a.50 < 50] <- 0
+  barplot(colMeans(a.50), las = 1, col = col, horiz = TRUE)
 }
 t <- dev.off()
 
