ad0129bc426bf8fb8e389a4dc88ff391542e4a47
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2019-10-06 02:26:42 -0400

    Update 08_02_gsp_qual.R

--- scripts/08_02_gsp_qual.R
+++ scripts/08_02_gsp_qual.R
@@ -8,7 +8,9 @@ for(i in argv){
   b <- barplot(t(a), beside = TRUE,
     col = c('lightblue', 'darkblue', 'red3', 'darkred'),
     ylab = 'Quality', main = i)
-  rect(1, 0, ceiling(max(b)), 50, border = 'black', col = rgb(0,0,0,1/3))
+  rect(1, 0, ceiling(max(b)), 50, border = NA, col = rgb(1,1,1,1/2))
+  arrows(x0=1, x1=ceiling(max(b)), y0=c(0,50), y1=c(0,50), lty = c(1,3),
+    length = 0)
 }
 t <- dev.off()
 
