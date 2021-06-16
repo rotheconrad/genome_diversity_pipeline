5f4413112ec59fa9f1b9fe65728f5b2738e81627
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2021-06-16 16:26:32 +0200

    Change variable name to fix conflict with MiGA

--- 07_derep.bash
+++ 07_derep.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-target=$1
-dataset=$2
+# Change `$dataset` variable to `$gd_dataset` to avoid conflicts with
+# MiGA's `$DATASET`
+
+gd_target=$1
+gd_dataset=$2
 scr=$(readlink -f "$0")
 pkg=$(dirname "$scr")
 
-if [[ ! -n $target || ! -n $dataset ]] ; then
+if [[ ! -n $gd_target || ! -n $gd_dataset ]] ; then
   echo "Usage: $0 target_folder dataset
   target_folder Path to the target folder containing the data
   dataset       Name of the dataset to process
@@ -15,17 +18,17 @@ fi
 
 # We don't need the genome_diversity_pipeline environment for this one
 #. "$pkg/00_env.bash"
-cd "$target"
+cd "$gd_target"
 
-dir="07_derep/$dataset"
+dir="07_derep/$gd_dataset"
 
 miga new -P "$dir" -t genomes
 miga add -P "$dir" -t popgenome -i assembly \
   -R '^(?:.*\/)?(.+?)(?i:\.f[nastq]+)?$' \
-  --prefix maxbin_ -v 05_maxbin/"$dataset"-*/*.fasta
+  --prefix maxbin_ -v 05_maxbin/"$gd_dataset"-*/*.fasta
 miga add -P "$dir" -t popgenome -i assembly \
   -R '^(?:.*\/)?(.+?)(?i:\.f[nastq]+)?$' \
-  --prefix metabat_ -v 06_metabat/"$dataset"-*/*.fa
+  --prefix metabat_ -v 06_metabat/"$gd_dataset"-*/*.fa
 
 miga derep_wf -o "$dir" --fast -j 12 -t 1 -v 
 
@@ -34,4 +37,4 @@ eval "$(miga env)"
 "$pkg/scripts/07_01_gsp_qual.rb" "$dir" > "$dir/method_qual.tsv"
 
 # Launch next step
-"$pkg/00_launcher.bash" . "$dataset" 08
+"$pkg/00_launcher.bash" . "$gd_dataset" 08
